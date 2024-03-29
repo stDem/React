d3e3db682fbf1dc633b48f05c07e065892c22c2c
stDem <61629062+stDem@users.noreply.github.com>
2024-03-29 19:31:26 +0300

    finished hw1

---
+++ homework1/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Message from './components/Message';
+import './components/style.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Message message="Homework1"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
